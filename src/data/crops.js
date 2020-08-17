f144c6f279d717d39c5d3c52a4f4057facc5bcc6
Farai Gandiya <gandiyafarai@gmail.com>
2020-08-17 19:51:45 +0200

    Turned crops json to JS
    Squashed commit of the following:
    
    commit a9a24ceafd59f7f4cf102817c6ea8904cd2e431a
    Author: Farai Gandiya <gandiyafarai@gmail.com>
    Date:   Mon Aug 17 19:49:23 2020 +0200
    
        Wrote parsed crops to /src/data/crops.js
    
    commit 165d4fcda35beed4e92312483452c70e71120a99
    Author: Farai Gandiya <gandiyafarai@gmail.com>
    Date:   Mon Aug 17 19:42:02 2020 +0200
    
        Removed find buying/selling price function.
    
        I'm optimizing too much, I can solve this at a higher level. For now,
        I'll just include buying/selling price wholesale
    
    commit 7ce2e457305639e36680d4f29079a5af47022267
    Author: Farai Gandiya <gandiyafarai@gmail.com>
    Date:   Mon Aug 17 19:35:07 2020 +0200
    
        Added function to get best selling price
    
    commit 8c4de38cf512e9665d8b2e88df9424204747b834
    Author: Farai Gandiya <gandiyafarai@gmail.com>
    Date:   Mon Aug 17 19:34:48 2020 +0200
    
        Created function to find buying price

---
+++ src/data/crops.js
@@ -0,0 +1,362 @@
+module.exports.Crops = [
+  {
+    name: 'Blue Jazz',
+    seasons: [ 'spring' ],
+    seed: 'Jazz Seeds',
+    buyingPrice: { pierre: 30, joja: 37, cart: '100-1000', nightMarket15: 30 },
+    sellingPrice: {
+      base: { regular: 50, silver: 62, gold: 75 },
+      tiller: { regular: 55, silver: 68, gold: 82 }
+    }
+  },
+  {
+    name: 'Cauliflower',
+    seasons: [ 'spring' ],
+    seed: 'Cauliflower Seeds',
+    buyingPrice: { pierre: 80, joja: 100, cart: '120-1000', nightMarket15: 80 },
+    sellingPrice: {
+      base: { regular: 175, silver: 218, gold: 262 },
+      tillers: { regular: 192, silver: 240, gold: 288 }
+    }
+  },
+  {
+    name: 'Garlic',
+    seasons: [ 'spring' ],
+    seed: 'Garlic Seeds',
+    buyingPrice: { pierre: 40, cart: '100-1000', nightMarket15: 40 },
+    sellingPrice: {
+      base: { regular: 60, silver: 75, gold: 90 },
+      tiller: { regular: 66, silver: 82, gold: 99 }
+    }
+  },
+  {
+    name: 'Kale',
+    seasons: [ 'spring' ],
+    seed: 'Kale Seeds',
+    buyingPrice: { pierre: 70, joja: 87, cart: '105-1000', nightMarket15: 70 },
+    sellingPrice: {
+      base: { regular: 110, silver: 137, gold: 165 },
+      tiller: { regular: 121, silver: 151, gold: 181 }
+    }
+  },
+  {
+    name: 'Parsnip',
+    seasons: [ 'spring' ],
+    seed: 'Parsnip Seed',
+    buyingPrice: { pierre: 20, cart: '100-1000', joja: 25, nightMarket15: 20 },
+    sellingPrice: {
+      base: { regular: 35, silver: 43, gold: 52 },
+      tiller: { regular: 38, silver: 47, gold: 57 }
+    }
+  },
+  {
+    name: 'Potato',
+    seasons: [ 'spring' ],
+    seed: 'Potato Seeds',
+    buyingPrice: { pierre: 50, joja: 62, cart: '100-1000', nightMarket15: 50 },
+    sellingPrice: {
+      base: { regular: 80, silver: 100, gold: 120 },
+      tiller: { regular: 88, silver: 110, gold: 132 }
+    }
+  },
+  {
+    name: 'Rhubab',
+    seasons: [ 'spring' ],
+    seed: 'Rhubab Seeds',
+    buyingPrice: { oasis: 100, cart: '150-1100' },
+    sellingPrice: {
+      base: { regular: 220, silver: 275, gold: 330 },
+      tiller: { regular: 242, silver: 302, gold: 363 }
+    }
+  },
+  {
+    name: 'Tulip',
+    seasons: [ 'spring' ],
+    seed: 'Tulip Bulb',
+    buyingPrice: { joja: 25, pierre: 20, cart: '100-1000', nightMarket15: 20 },
+    sellingPrice: {
+      base: { regular: 30, silver: 37, gold: 45 },
+      tiller: { regular: 33, silver: 41, gold: 49 }
+    }
+  },
+  {
+    name: 'Unmilled Rice',
+    seasons: [ 'spring' ],
+    seed: 'Rice Shoot',
+    buyingPrice: { pierre: 40, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 30, silver: 37, gold: 45 },
+      tiller: { regular: 33, silver: 41, gold: 49 }
+    }
+  },
+  {
+    name: 'Coffee Bean',
+    seasons: [ 'spring', 'summer' ],
+    seed: 'Coffee Bean',
+    buyingPrice: { cart: '100-1000', cartSpecial: 2500 },
+    sellingPrice: {
+      base: { regular: 15, silver: 18, gold: 22 },
+      tiller: { regular: 15, silver: 18, gold: 22 }
+    }
+  },
+  {
+    name: 'Green Bean',
+    seasons: [ 'spring' ],
+    seed: 'Bean Starter',
+    buyingPrice: { pierre: 60, joja: 75, cart: '100-1000', nightMarket15: 60 },
+    sellingPrice: {
+      base: { regular: 40, silver: 50, gold: 60 },
+      tiller: { regular: 44, silver: 55, gold: 66 }
+    }
+  },
+  {
+    name: 'Strawberry',
+    seasons: [ 'spring' ],
+    seed: 'Strawberry Seeds',
+    buyingPrice: { eggFestival: 100 },
+    sellingPrice: {
+      base: { regular: 120, silver: 150, gold: 180 },
+      tiller: { regular: 132, silver: 165, gold: 198 }
+    }
+  },
+  {
+    name: 'Melon',
+    seasons: [ 'summer' ],
+    seed: 'Melon Seeds',
+    buyingPrice: { joja: 100, pierre: 80, cart: '120-1000', nightMarket16: 80 },
+    sellingPrice: {
+      base: { regular: 250, silver: 312, gold: 375 },
+      tiller: { regular: 275, silver: 343, gold: 412 }
+    }
+  },
+  {
+    name: 'Poppy',
+    seasons: [ 'summer' ],
+    seed: 'Poppy Seeds',
+    buyingPrice: { pierre: 100, nightMarket16: 100, joja: 125, cart: '150-1000' },
+    sellingPrice: {
+      base: { regular: 140, silver: 175, gold: 210 },
+      tiller: { regular: 154, silver: 192, gold: 231 }
+    }
+  },
+  {
+    name: 'Radish',
+    seasons: [ 'summer' ],
+    seed: 'Radish Seeds',
+    buyingPrice: { joja: 50, pierre: 40, nightMarket16: 40, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 90, silver: 112, gold: 135 },
+      tiller: { regular: 99, silver: 123, gold: 148 }
+    }
+  },
+  {
+    name: 'Red Cabbage',
+    seasons: [ 'summer' ],
+    seed: 'Red Cabbage Seeds',
+    buyingPrice: { pierre: 100, nightMarket16: 100, cart: '150-1000' },
+    sellingPrice: {
+      base: { regular: 260, silver: 325, gold: 390 },
+      tiller: { regular: 286, silver: 357, gold: 429 }
+    }
+  },
+  {
+    name: 'Starfruit',
+    seasons: [ 'summer' ],
+    seed: 'Starfruit Seeds',
+    buyingPrice: { oasis: 400, cart: '600-1000' },
+    sellingPrice: {
+      base: { regular: 750, silver: 937, gold: 1125 },
+      tiller: { regular: 825, silver: 1031, gold: 1237 }
+    }
+  },
+  {
+    name: 'Summer Spangle',
+    seasons: [ 'summer' ],
+    seed: 'Spangle Seeds',
+    buyingPrice: { joja: 62, pierre: 50, nightMarket16: 50, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 90, silver: 112, gold: 135 },
+      tiller: { regular: 99, silver: 123, gold: 148 }
+    }
+  },
+  {
+    name: 'Sunflower',
+    seasons: [ 'summer', 'fall' ],
+    seed: 'Sunflower Seeds',
+    buyingPrice: { joja: 125, pierre: 200, nightMarket16: 200, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 80, silver: 100, gold: 120 },
+      tiller: { regular: 88, silver: 110, gold: 132 }
+    }
+  },
+  {
+    name: 'Wheat',
+    seasons: [ 'summer', 'fall' ],
+    seed: 'Wheat Seeds',
+    buyingPrice: { joja: 12, pierre: 10, nightMarket16: 10, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 25, silver: 31, gold: 37 },
+      tiller: { regular: 27, silver: 33, gold: 40 }
+    }
+  },
+  {
+    name: 'Blueberry',
+    seasons: [ 'summer' ],
+    seed: 'Blueberry Seeds',
+    buyingPrice: { pierre: 80, nightMarket16: 80, cart: '120-1000' },
+    sellingPrice: {
+      base: { regular: 50, silver: 62, gold: 75 },
+      tiller: { regular: 55, silver: 68, gold: 82 }
+    }
+  },
+  {
+    name: 'Corn',
+    seasons: [ 'summer', 'fall' ],
+    seed: 'Corn Seeds',
+    buyingPrice: { pierre: 150, nightMarket16: 150, cart: '225-1000' },
+    sellingPrice: {
+      base: { regular: 50, silver: 62, gold: 75 },
+      tiller: { regular: 55, silver: 68, gold: 82 }
+    }
+  },
+  {
+    name: 'Hops',
+    seasons: [ 'summer' ],
+    seed: 'Hops Starter',
+    buyingPrice: { joja: 75, pierre: 60, nightMarket16: 60, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 25, silver: 31, gold: 37 },
+      tiller: { regular: 27, silver: 33, gold: 40 }
+    }
+  },
+  {
+    name: 'Hot Pepper',
+    seasons: [ 'summer' ],
+    seed: 'Pepper Seeds',
+    buyingPrice: { joja: 50, pierre: 40, nightMarket16: 40, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 40, silver: 50, gold: 60 },
+      tiller: { regular: 44, silver: 55, gold: 66 }
+    }
+  },
+  {
+    name: 'Tomato',
+    seasons: [ 'summer' ],
+    seed: 'Tomato Seeds',
+    buyingPrice: { joja: 62, pierre: 50, nightMarket16: 50, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 60, silver: 75, gold: 90 },
+      tiller: { regular: 66, silver: 82, gold: 99 }
+    }
+  },
+  {
+    name: 'Amaranth',
+    seasons: [ 'fall' ],
+    seed: 'Amaranth Seeds',
+    buyingPrice: { joja: 87, pierre: 70, nightMarket17: 70, cart: '105-1000' },
+    sellingPrice: {
+      base: { regular: 150, silver: 187, gold: 225 },
+      tiller: { regular: 165, silver: 206, gold: 247 }
+    }
+  },
+  {
+    name: 'Artichoke',
+    seasons: [ 'fall' ],
+    seed: 'Artichoke Seeds',
+    buyingPrice: { pierre: 30, nightMarket17: 30, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 160, silver: 200, gold: 240 },
+      tiller: { regular: 176, silver: 220, gold: 264 }
+    }
+  },
+  {
+    name: 'Beet',
+    seasons: [ 'fall' ],
+    seed: 'Beet Seeds',
+    buyingPrice: { oasis: 20, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 100, silver: 125, gold: 150 },
+      tiller: { regular: 110, silver: 137, gold: 165 }
+    }
+  },
+  {
+    name: 'Bok Choy',
+    seasons: [ 'fall' ],
+    seed: 'Bok Choy Seeds',
+    buyingPrice: { joja: 62, pierre: 50, nightMarket17: 50, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 80, silver: 100, gold: 120 },
+      tiller: { regular: 88, silver: 110, gold: 132 }
+    }
+  },
+  {
+    name: 'Fairy Rose',
+    seasons: [ 'fall' ],
+    seed: 'Fairy Seeds',
+    buyingPrice: { joja: 250, pierre: 200, nightMarket17: 200, cart: '300-1000' },
+    sellingPrice: {
+      base: { regular: 290, silver: 362, gold: 435 },
+      tiller: { regular: 319, silver: 398, gold: 478 }
+    }
+  },
+  {
+    name: 'Pumpkin',
+    seasons: [ 'fall' ],
+    seed: 'Pumpkin Seeds',
+    buyingPrice: { joja: 125, pierre: 100, nightMarket17: 100, cart: '150-1000' },
+    sellingPrice: {
+      base: { regular: 320, silver: 400, gold: 480 },
+      tiller: { regular: 352, silver: 440, gold: 528 }
+    }
+  },
+  {
+    name: 'Sweet Gem Berry',
+    seasons: [ 'fall' ],
+    seed: 'Rare Seed',
+    buyingPrice: { cart: '600-1000', cartSpecial: 1000 },
+    sellingPrice: {
+      base: { regular: 3000, silver: 3750, gold: 4500 },
+      tiller: { regular: 3000, silver: 3750, gold: 4500 }
+    }
+  },
+  {
+    name: 'Yam',
+    seasons: [ 'fall' ],
+    seed: 'Yam Seeds',
+    buyingPrice: { joja: 75, pierre: 60, nightMarket17: 60, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 160, silver: 200, gold: 240 },
+      tiller: { regular: 176, silver: 220, gold: 264 }
+    }
+  },
+  {
+    name: 'Cranberries',
+    seasons: [ 'fall' ],
+    seed: 'Cranberry Seeds',
+    buyingPrice: { joja: 300, pierre: 240, nightMarket17: 240, cart: '360-1000' },
+    sellingPrice: {
+      base: { regular: 75, silver: 93, gold: 112 },
+      tiller: { regular: 82, silver: 102, gold: 123 }
+    }
+  },
+  {
+    name: 'Eggplant',
+    seasons: [ 'fall' ],
+    seed: 'Eggplant Seeds',
+    buyingPrice: { joja: 25, pierre: 20, nightMarket17: 20, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 60, silver: 75, gold: 90 },
+      tiller: { regular: 66, silver: 82, gold: 99 }
+    }
+  },
+  {
+    name: 'Grape',
+    seasons: [ 'fall' ],
+    seed: 'Grape Starter',
+    buyingPrice: { joja: 75, pierre: 60, nightMarket17: 60, cart: '100-1000' },
+    sellingPrice: {
+      base: { regular: 80, silver: 100, gold: 120, iridium: 176 },
+      tiller: { regular: 88, silver: 110, gold: 132, iridium: 176 }
+    }
+  }
+]
---
+++ utils/crop-loader.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const DATA_DIR = path.join(__dirname, '../src/data/')
+const DATA_SRC = path.join(DATA_DIR, 'crops.json')
+const DATA_DEST = path.join(DATA_DIR, 'crops.js')
+
+function main () {
+    const cropData = fs.readFileSync(DATA_SRC, { encoding: 'utf-8' })
+    const cropsObj = JSON.parse(cropData)
+    const parsedCrops = []
+    for (const crop of cropsObj) {
+        const cropObj = {
+            name: crop.name,
+            seasons: crop.seasons,
+            seed: crop.seed,
+            buyingPrice: crop.buyingPrice,
+            sellingPrice: crop.sellingPrice
+        }
+        parsedCrops.push(cropObj)
+    }
+    const cropsStr = util.inspect(parsedCrops, false, Infinity)
+    fs.writeFileSync(DATA_DEST, `module.exports.Crops = ${cropsStr}\n`)
+}
+
+main()
