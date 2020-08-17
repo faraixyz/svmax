8c4de38cf512e9665d8b2e88df9424204747b834
Farai Gandiya <gandiyafarai@gmail.com>
2020-08-17 19:34:48 +0200

    Created function to find buying price

---
+++ utils/crop-loader.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+const DATA_DIR = path.join(__dirname, '../src/data/')
+const DATA_SRC = path.join(DATA_DIR, 'crops.json')
+const DATA_DEST = path.join(DATA_DIR, 'crops.js')
+
+function findBuyingPrice (prices) {
+    let min = []
+    for (const store of Object.keys(prices)) {
+        if (store === 'cart') continue;
+        if (min[0] !== undefined) {
+            switch (true) {
+                case prices[store] < min[0].price:
+                    min = [{store, price:prices[store]}]
+                    break
+                case prices[store] === min[0].price:
+                    min.push({store, price:prices[store]})
+                    break
+            }
+        } else {
+            min.push({store, price:prices[store]})
+        }
+    }
+    return min
+}
+
+function main () {
+    const cropData = fs.readFileSync(DATA_SRC, { encoding: 'utf-8' })
+    const cropsObj = JSON.parse(cropData)
+    const parsedCrops = []
+    for (const crop of cropsObj) {
+        const cropObj = {
+            name: crop.name,
+            seasons: crop.seasons,
+            seed: crop.seed
+        }
+        cropObj.buyingPrice = findBuyingPrice(crop.buyingPrice)
+        cropObj.sellingPrice = findSellingPrice(crop.sellingPrice)
+        parsedCrops.push(cropObj)
+    }
+    console.log(parsedCrops)
+}
+
+main()
